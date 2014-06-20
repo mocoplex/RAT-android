bc1e64393b4239e8d5dc60f547b4351d851cf5b3
YongSun Kim <yongsunkim@YongSunui-MacBook-Pro.local>
2014-06-20 15:52:17 +0900

    update sdk and clean up comment.

--- src/com/mocoplex/rat/sample/MainActivity.java
+++ src/com/mocoplex/rat/sample/MainActivity.java
@@ -1,5 +1,8 @@
 package com.mocoplex.rat.sample;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
@@ -14,20 +17,26 @@ import com.mocoplex.rat.Tracker;
 
 public class MainActivity extends Activity {
 
-	private WebView m_webView = null;  
-	 private Handler m_handler = null;
-	 private boolean mFlag = false;
+	// 웹뷰
+	private WebView m_webView = null; 
+	
+	// Back Key 관련 핸들러
+	private Handler m_handler = null;
+	// Back Key 관련 플래그
+	private boolean mFlag = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		// 트래킹 연동을 위해 필수로 호출
+		// 트래킹 연동을 위해 필수로 호출되어야 합니다. (인스톨 트래킹)
 		Tracker.getInstance().init(this);
-		// 트래킹 연동을 위해 필수로 호출
 		
-		//커스텀 스키마로 들어온 경우 파라미터 확인 
+		// 트래킹 연동을 위해 필수로 호출되어야 합니다. (태그 전환 트래킹)
+		Tracker.getInstance().init_tag(this, getIntent().getData());
+		
+		// 커스텀 스키마에 대해 처리를 합니다. (옵션)
 		parseCustomScheme();
 		
 		m_handler = new Handler() {
@@ -41,36 +50,26 @@ public class MainActivity extends Activity {
 	}
 	
 	protected void parseCustomScheme() {
-
 		Uri data = getIntent().getData();
-       if(data != null) { // custom schema 통해 들어온 경우
-       	
-           // 태그 전환 확인을 위해서는 아래 내용이 꼭 먼저 호출되어야 합니다.
-           String ratparam = data.getQueryParameter("adlib_ratparam");
-           if(ratparam != null && !ratparam.equals(""))
-           {
-           	Tracker.getInstance().init_tag(this, ratparam);
-           }
-           // 구현 끝.
-           
+       if(data != null) { // custom schema 통해 들어온 경우           
            // EX) rat://view?pid=123;
            String pid = data.getQueryParameter("pid");          
 
            if(pid != null && !pid.equals("")){
-           		// 상세 웹 페이지로 이동
+        	   // 상세 페이지로 이동
         	   showDetail(pid);
            }else{
-           		// 메인 웹 페이지로 이동
+        	   // 메인 페이지로 이동
         	   showMain();
            }
-       } else {
-       		// 메인 웹 페이지로 이동
+       }else{
+    	   // 메인 페이지로 이동
     	   showMain();
        }
        		
 	}
 	
-	/* 메인 웹 페이지로 이동 */
+	// 메인 웹 페이지로 이동
 	protected void showMain() {
 		m_webView = (WebView)findViewById(R.id.webView);
 		m_webView.getSettings().setJavaScriptEnabled(true);
@@ -79,7 +78,7 @@ public class MainActivity extends Activity {
 		m_webView.loadUrl("http://demo.mocoplex.com/rat/index.html");
 	}
 	
-	/* 상세 웹 페이지로 이동 */
+	// 상세 웹 페이지로 이동
 	protected void showDetail(String pid) {
 		m_webView = (WebView)findViewById(R.id.webView);
 		m_webView.getSettings().setJavaScriptEnabled(true);
@@ -102,27 +101,54 @@ public class MainActivity extends Activity {
 	// 웹 페이지의 자바스크립트 연동
 	class JavaScriptInterface {
 		
-		Context mContext;
+		Context context;
 		
-		JavaScriptInterface(Context c) {
-			mContext = c;
+		JavaScriptInterface(Context ctx) {
+			context = ctx;
 		}
 		
 		// 상세 페이지 로깅
-		public void view(String id) {
-			Tracker.getInstance().view(mContext, id);
+		public void view(String pid) {
+			// RAT SDK 호출 (view)
+			Tracker.getInstance().view(context, pid);
 		}
 		
 		// 카트 로깅
-		public void cart(String id) {
-			Toast.makeText(mContext, "Item added to Cart.", Toast.LENGTH_SHORT).show();
-			Tracker.getInstance().cart(mContext, id);
+		public void cart(String pid) {
+			Toast.makeText(context, "Item added to Cart.", Toast.LENGTH_SHORT).show();
+			
+			// RAT SDK 호출 (cart)
+			Tracker.getInstance().cart(context, pid);
 		}
 		
 		// 구매 로깅
-		public void buy(String id) {
-			Toast.makeText(mContext, "Thank you.", Toast.LENGTH_SHORT).show();
-			Tracker.getInstance().buy(mContext, id);
+		public void buy(String pid) {
+			Toast.makeText(context, "Thank you.", Toast.LENGTH_SHORT).show();
+			
+			// RAT SDK 호출 (buy)
+			Tracker.getInstance().buy(context, pid);
+		}
+		
+		// 사용자정의 태그 로깅
+		public void customTag(String pid) {
+			
+			JSONObject v = new JSONObject();
+			
+			try {
+				// 사용자 정의 추가 변수 저장
+				v.put("pid", pid);
+				v.put("category", "cloth");
+				v.put("price", "1000000");
+				v.put("qty", "10");
+				v.put("name", "청바지");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			
+			// RAT SDK 호출 (custom tag)
+			// 태그 이름 : TAG_NAME
+			// 추가 parameter : v (json)
+			Tracker.getInstance().tag(context, "TAG_NAME", v);
 		}
 	}
 	
