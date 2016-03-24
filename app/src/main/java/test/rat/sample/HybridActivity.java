13ed570f35817abe6533cf67f09a85e4652f4a2d
berrybana <hana@mocoplex.com>
2016-03-24 17:36:19 +0900

    update sample project

--- app/src/main/java/test/rat/sample/HybridActivity.java
+++ app/src/main/java/test/rat/sample/HybridActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.mocoplex.rat.ADLIBrHybridInterface;
